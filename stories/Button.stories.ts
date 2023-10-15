a7c0723dd429350ec3e75c63a0ce725587673f4d
Patrick Lehmann <patrick.lehmann@intergga.ch>
2023-10-15 21:56:23 +0200

    chore: change type declaration in button stories file

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -2,8 +2,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '../components/button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-const meta = {
-  title: 'Example/Button',
+const meta: Meta<typeof Button> = {
+  title: 'Components/Button',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
@@ -15,7 +15,7 @@ const meta = {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} satisfies Meta<typeof Button>;
+}
 
 export default meta;
 type Story = StoryObj<typeof meta>;
