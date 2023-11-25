48a883718379463f497011f8ecd9774cf019866e
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2023-11-25 22:21:56 +0100

    feat: sets icons as per figma design

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -15,6 +15,36 @@ const config: StorybookConfig = {
     name: '@storybook/nextjs',
     options: {},
   },
+  webpackFinal: async (config) => {
+    if (config.module?.rules) {
+      const imageRule = config.module?.rules?.find((rule) => {
+        const test = (rule as { test: RegExp }).test;
+
+        if (!test) {
+          return false;
+        }
+
+        return test.test('.svg');
+      }) as { [key: string]: any };
+
+      imageRule.exclude = /\.svg$/;
+
+      config.module?.rules?.push({
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+            },
+          },
+        ],
+      });
+
+      return config;
+    }
+    return config;
+  },
   typescript: {
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
--- components/icon/Icon.tsx
+++ components/icon/Icon.tsx
@@ -9,13 +9,31 @@
 import React, { useMemo } from 'react';
 import './icon.css';
 import { EIConTypes } from './icon.enum';
-import {
-  ArrowLeftIcon,
-  ArrowRightIcon,
-  ArrowDownIcon,
-  ArrowUpIcon,
-} from '@heroicons/react/24/solid';
 import { IIconProps } from './icon.interface';
+import MumbleIcon from './svg/mumble.svg';
+import ArrowLeftIcon from './svg/arrow-left.svg';
+import ArrowRightIcon from './svg/arrow-right.svg';
+import ArrowUpIcon from './svg/arrow-up.svg';
+import ArrowDownIcon from './svg/arrow-down.svg';
+import CalendarIcon from './svg/calendar.svg';
+import CancelIcon from './svg/cancel.svg';
+import CheckmarkIcon from './svg/checkmark.svg';
+import CommentBorderedIcon from './svg/comment-bordered.svg';
+import CommentFilledIcon from './svg/comment-filled.svg';
+import EditIcon from './svg/edit.svg';
+import EyeIcon from './svg/eye.svg';
+import FullScreenIcon from './svg/full-screen.svg';
+import HeardBorderedIcon from './svg/heart-bordered.svg';
+import HeartFilledIcon from './svg/heart-filled.svg';
+import LocationIcon from './svg/location.svg';
+import LogoutIcon from './svg/logout.svg';
+import ProfileIcon from './svg/profile.svg';
+import RepostIcon from './svg/repost.svg';
+import SendIcon from './svg/send.svg';
+import SettingsIcon from './svg/settings.svg';
+import ShareIcon from './svg/share.svg';
+import TimeIcon from './svg/time.svg';
+import UploadIcon from './svg/upload.svg';
 
 const Icon = ({ type, size, color }: IIconProps) => {
   const componentName = 'c-icon';
@@ -38,6 +56,8 @@ const Icon = ({ type, size, color }: IIconProps) => {
   }, [size]);
   const iconMarkup = useMemo(() => {
     switch (type) {
+      case EIConTypes.MUMBLE:
+        return <MumbleIcon />;
       case EIConTypes.ARROW_LEFT:
         return <ArrowLeftIcon />;
       case EIConTypes.ARROW_RIGHT:
@@ -46,6 +66,44 @@ const Icon = ({ type, size, color }: IIconProps) => {
         return <ArrowDownIcon />;
       case EIConTypes.ARROW_UP:
         return <ArrowUpIcon />;
+      case EIConTypes.CALENDAR:
+        return <CalendarIcon />;
+      case EIConTypes.CANCEL:
+        return <CancelIcon />;
+      case EIConTypes.CHECKMARK:
+        return <CheckmarkIcon />;
+      case EIConTypes.COMMENT_BORDERED:
+        return <CommentBorderedIcon />;
+      case EIConTypes.COMMENT_FILLED:
+        return <CommentFilledIcon />;
+      case EIConTypes.EDIT:
+        return <EditIcon />;
+      case EIConTypes.EYE:
+        return <EyeIcon />;
+      case EIConTypes.FULL_SCREEN:
+        return <FullScreenIcon />;
+      case EIConTypes.HEART_BORDERED:
+        return <HeardBorderedIcon />;
+      case EIConTypes.HEART_FILLED:
+        return <HeartFilledIcon />;
+      case EIConTypes.LOCATION:
+        return <LocationIcon />;
+      case EIConTypes.LOGOUT:
+        return <LogoutIcon />;
+      case EIConTypes.PROFILE:
+        return <ProfileIcon />;
+      case EIConTypes.REPOST:
+        return <RepostIcon />;
+      case EIConTypes.SEND:
+        return <SendIcon />;
+      case EIConTypes.SETTINGS:
+        return <SettingsIcon />;
+      case EIConTypes.SHARE:
+        return <ShareIcon />;
+      case EIConTypes.TIME:
+        return <TimeIcon />;
+      case EIConTypes.UPLOAD:
+        return <UploadIcon />;
     }
   }, [type]);
 
