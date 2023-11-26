26a7458346b8b6b6d0b1f761e182820efeab2941
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2023-11-26 16:22:06 +0100

    feat: removes usage of svgr package

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -15,37 +15,6 @@ const config: StorybookConfig = {
     name: '@storybook/nextjs',
     options: {},
   },
-  webpackFinal: async (config) => {
-    if (config.module?.rules) {
-      const imageRule = config.module?.rules?.find((rule) => {
-        const test = (rule as { test: RegExp }).test;
-
-        if (!test) {
-          return false;
-        }
-
-        return test.test('.svg');
-      }) as { [key: string]: any };
-
-      imageRule.exclude = /\.svg$/;
-
-      config.module?.rules?.push({
-        test: /\.svg$/,
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              typescript: true,
-              icon: true,
-            },
-          },
-        ],
-      });
-
-      return config;
-    }
-    return config;
-  },
   typescript: {
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
