89787b2490e0e5ad6606a7ca93940cb2920b75a5
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2023-11-26 21:36:21 +0100

    feat: removes size property of icon. adds type IconTypes

--- components/button/button.interface.ts
+++ components/button/button.interface.ts
@@ -4,17 +4,17 @@ import {
   EButtonSizes,
   EButtonTypes,
 } from './button.enum';
-import { EIConTypes } from '../icon/icon.enum';
+import { TIconType } from '../icon/icon.interface';
 
 export interface IButtonProps {
   type?: EButtonTypes.PRIMARY | EButtonTypes.SECONDARY | EButtonTypes.TERTIARY;
   size?: EButtonSizes.MEDIUM | EButtonSizes.SMALL | EButtonSizes.LARGE;
   label: string;
   onClickEvent: () => void;
-  icon?: EIConTypes;
+  icon?: TIconType;
   iconPosition?: EButtonIconPosition.LEFT | EButtonIconPosition.RIGHT;
   href?: string;
   disabled?: boolean;
-  kind?: EButtonKinds;
+  kind?: EButtonKinds.BUTTON | EButtonKinds.LINK;
   onlyIcon?: boolean;
 }
--- stories/Icon.stories.ts
+++ stories/Icon.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Icon from '../components/icon/Icon';
-import { EIconSizes, EIConTypes } from '../components/icon/icon.enum';
+import { EIConTypes } from '../components/icon/icon.enum';
 
 export const iconOptions = [
   EIConTypes.ARROW_DOWN,
@@ -35,10 +35,6 @@ const meta: Meta<typeof Icon> = {
   component: Icon,
   tags: ['autodocs'],
   argTypes: {
-    size: {
-      control: { type: 'select' },
-      options: [EIconSizes.LG, EIconSizes.MD, EIconSizes.SM],
-    },
     type: {
       control: { type: 'select' },
       options: iconOptions,
@@ -51,160 +47,137 @@ type Story = StoryObj<typeof meta>;
 
 export const IconArrowUp: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.ARROW_UP,
   },
 };
 export const IconArrowDown: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.ARROW_DOWN,
   },
 };
 
 export const IconArrowRight: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.ARROW_RIGHT,
   },
 };
 
 export const IconArrowLeft: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.ARROW_LEFT,
   },
 };
 
 export const IconCalendar: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.CALENDAR,
   },
 };
 
 export const IconCancel: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.CANCEL,
   },
 };
 
 export const IconCheckmark: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.CHECKMARK,
   },
 };
 
 export const IconCommentBordered: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.COMMENT_BORDERED,
   },
 };
 
 export const IconCommentFilled: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.COMMENT_FILLED,
   },
 };
 
 export const IconEdit: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.EDIT,
   },
 };
 
 export const IconEye: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.EYE,
   },
 };
 
 export const IconFullscreen: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.FULL_SCREEN,
   },
 };
 
 export const IconHeartBordered: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.HEART_BORDERED,
   },
 };
 
 export const IconHeartFilled: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.HEART_FILLED,
   },
 };
 
 export const IconHeartLocation: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.LOCATION,
   },
 };
 
 export const IconHeartLogout: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.LOGOUT,
   },
 };
 
 export const IconProfile: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.PROFILE,
   },
 };
 
 export const IconRepost: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.REPOST,
   },
 };
 
 export const IconSend: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.SEND,
   },
 };
 
 export const IconSettings: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.SETTINGS,
   },
 };
 
 export const IconShare: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.SHARE,
   },
 };
 
 export const IconTime: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.TIME,
   },
 };
 
 export const IconUpload: Story = {
   args: {
-    size: EIconSizes.LG,
     type: EIConTypes.UPLOAD,
   },
 };
