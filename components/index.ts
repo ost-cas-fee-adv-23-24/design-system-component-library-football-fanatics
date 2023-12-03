52aeaf11063737823a9388077084a7199b664ab7
Patrick Lehmann <patrick.lehmann@intergga.ch>
2023-12-03 12:42:04 +0100

    export InputFieldGroup component

--- components/index.ts
+++ components/index.ts
@@ -4,6 +4,7 @@ export * from './button/Button';
 export { default as Button } from './button/Button';
 export * from './icon/Icon';
 export { default as Icon } from './icon/Icon';
+export { InputFieldGroup } from './input-group/InputFieldGroup';
 export { Label } from './label/Label';
 export { Heading } from './typography/Heading';
 export { Paragraph } from './typography/Paragraph';
