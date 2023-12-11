bf24e51940074737e1f2d95b9db9cad0841430bf
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2023-12-11 09:48:12 +0100

    fix: adapts returning value on image selected

--- components/avatar/avatar.interface.ts
+++ components/avatar/avatar.interface.ts
@@ -4,10 +4,7 @@ import { EAvatarSizes } from './avata.enum';
 export interface IAvatarComponentProps {
   imgSrc?: string;
   name?: string;
-  onSuccess: (data: {
-    file: File;
-    fileData: { name: string; type: string; size: number };
-  }) => void;
+  onSuccess: (file: File) => void;
   onError: (error: Error) => void;
   editable: boolean;
   size: EAvatarSizes.LG | EAvatarSizes.MD | EAvatarSizes.SM | EAvatarSizes.XL;
