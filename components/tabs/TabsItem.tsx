34d1e2e4b35783d29e1ed1c6caeab4173d22e3e9
Patrick Lehmann <patrick.lehmann@intergga.ch>
2023-12-11 09:35:47 +0100

    remove <label> component from button

--- components/tabs/TabsItem.tsx
+++ components/tabs/TabsItem.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
 import React from 'react';
-import { Label } from '../label/Label';
 
 export interface ITabsItemProps {
   isActive: boolean;
@@ -8,27 +7,20 @@ export interface ITabsItemProps {
   onClick?: () => void;
 }
 
-export const TabsItem = ({
-  isActive,
-  text,
-  onClick,
-}: ITabsItemProps) => {
+export const TabsItem = ({ isActive, text, onClick }: ITabsItemProps) => {
   return (
-    <li className={clsx(
-      'cursor-pointer rounded-md py-[10px] px-[14px]',
-      isActive ? 'text-violet-600 bg-white ' : 'text-slate-600 hover:text-slate-800',
-
-    )}>
-      <button className={clsx(
-      )
-      }
-        onClick={onClick}>
-        <Label size='L' text={text} />
-
+    <li
+      className={clsx(
+        'cursor-pointer rounded-md py-[10px] px-[14px]',
+        'text-xl not-italic font-semibold leading-none text-current font-poppins',
+        isActive
+          ? 'text-violet-600 bg-white '
+          : 'text-slate-600 hover:text-slate-800',
+      )}
+    >
+      <button aria-label="Tab Item" onClick={onClick}>
+        {text}
       </button>
     </li>
-
-
-
   );
 };
