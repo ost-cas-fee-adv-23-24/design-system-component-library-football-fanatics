5972948616c303f4ff389523cd04abb136b55dbf
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2024-02-04 16:05:14 +0100

    feat: corrects paddings on buttonIcon

--- src/components/button/ButtonIcon.tsx
+++ src/components/button/ButtonIcon.tsx
@@ -11,7 +11,7 @@ export const ButtonIcon = ({
   type,
 }: IPropsButtonIcon) => {
   let cssClasses =
-    'rounded px-8 flex items-center justify-center font-poppins  text-sm not-italic font-semibold leading-3 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:text-gray-300 transition-all ease-in delay-50 rounded disabled:bg-gray-200';
+    'rounded flex items-center justify-center font-poppins  text-sm not-italic font-semibold leading-3 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:text-gray-300 transition-all ease-in delay-50 rounded disabled:bg-gray-200';
 
   if (type === 'secondary') {
     cssClasses = `${cssClasses} text-slate-400 hover:text-slate-600`;
